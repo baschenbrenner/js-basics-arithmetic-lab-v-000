1bfbf3c86f82690bf0ad68d4bc6d4ade85608c3d
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-12-30 22:05:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ newID = 1000000000 + oldID
 ageIsValid = Number.isInteger(currentAge)
 
 randomNumber = Math.random()*20
-randomInteger = Math.round(randomNumber)
+randomInteger = Math.floor(randomNumber)
